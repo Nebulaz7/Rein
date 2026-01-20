271c70f5f3b63622c6e3107186f1c0a7d99a728c
Nebulaz7 <jp0712002@gmail.com>
2026-01-20 13:49:56 +0100

    feat: Refactor Navbar and add Hero component for improved layout

--- rein-frontend/app/Components/Navbar.tsx
+++ rein-frontend/app/Components/Navbar.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
+import Link from "next/link";
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <div>
-      <h1>Navbar</h1> <button>signin</button>
-    </div>
+    <header className="pb-2 sticky top-0 w-full shrink-0 bg-muted/50 backdrop-blur-xl z-30">
+      <div className="flex justify-between items-center gap-5 w-full max-w-7xl px-2 md:px-6 h-[3.5rem] mx-auto">
+        <div
+          role="link"
+          tabIndex={0}
+          className="select-none cursor-pointer outline-0"
+        >
+          <Link href="/" className="text-[24px] pl-2">
+            Rein{" "}
+            {/* <span className="text-sm text-white  p-[6px] border-solid border-[1.3px] border-white rounded-full">
+              Beta
+            </span> */}
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            href="/get-started"
+            className="appearance-none cursor-pointer px-6 py-1 font-heading rounded-full text-sm bg-green-500/10 border border-green-500/20 hover:bg-green-500/20 focus-visible:bg-white/20 text-green-500 backdrop-blur-sm transition-all duration-200"
+          >
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
-export default Navbar;
+export default NavBar;
--- rein-frontend/app/page.tsx
+++ rein-frontend/app/page.tsx
@@ -1,16 +1,11 @@
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Rein</h1>
-      <p className="text-xl md:text-2xl text-muted-foreground max-w-xl">
-        Your AI-powered execution agent for{" "}
-        <span className="text-foreground font-semibold">
-          resolutions that stick
-        </span>
-      </p>
+      <Hero />
     </div>
   );
 }
